d456f63d86dbea63e893b9cfe50ad7458248d640
Eric Liu <eric.liu.developer@gmail.com>
2014-08-20 13:48:15 +0800

    parent class

--- src/com/playactionmenu/BaseActivity.java
+++ src/com/playactionmenu/BaseActivity.java
@@ -6,9 +6,12 @@ import android.view.ActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
+import android.view.MenuItem.OnMenuItemClickListener;
+import android.widget.Toast;
 
-public class BaseActivity extends Activity {
+public  class BaseActivity extends Activity {
 	
 	private Menu menu;
 
@@ -17,7 +20,9 @@ public class BaseActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_base, menu);
 		this.menu = menu;
-		menu.findItem(R.id.action_create).setActionProvider(new MyActionProvider(this));
+//		menu.findItem(R.id.action_create).setActionProvider(new MyActionProvider(this));
+		
+		
 		return true;
 	}
 
@@ -34,33 +39,71 @@ public class BaseActivity extends Activity {
 	}
 	
 	
-	protected void showMenuItems(int[] ids) {
-		for(int id: ids){
-			menu.findItem(id).setVisible(true);
-		}
+	protected void showMenuItem(int menuItemId, String label, String[] subMenuTitles) {
+			MenuItem item = menu.findItem(menuItemId);
+			item.setVisible(true);
+			if (label != null) {
+				item.setTitle(label);
+				item.setIcon(null);
+			}
+			if (subMenuTitles != null && subMenuTitles.length > 0) {
+				item.setActionProvider(new MyActionProvider(this, subMenuTitles));
+			}
+	}
+	
+	protected  void onSubMenuItemClicked(int itemId){
+		
+	};
+	
+	
+	
+	
+	
+	
+private class MyActionProvider extends ActionProvider implements OnMenuItemClickListener{
+	private Context context;
+	private String[] titles = {"First" , "Second", "Third" , "Fourth", " Fifth"};
+	
+	public MyActionProvider(Context context, String[] subMenuTitles) {
+		super(context);
+		this.context = context;
+		titles = subMenuTitles;
+	}
+
+	@Override
+	public View onCreateActionView() {
+//		LayoutInflater inflater = LayoutInflater.from(context);
+//		View rootView = inflater.inflate(R.layout.action_submenu_create, null);
+		
+		
+		return null;
 	}
 	
 	
+	@Override
+	public boolean hasSubMenu() {
+		
+		return true;
+	}
 	
 	
+	@Override
+	public void onPrepareSubMenu(SubMenu subMenu) {
+		subMenu.clear();
+		
+		for (int i = 0; i < titles.length; i++) {
+			subMenu.add(0, i, i, titles[i]).setIcon(R.drawable.ic_launcher).setOnMenuItemClickListener(this);
+		}
+	}
+
+	@Override
+	public boolean onMenuItemClick(MenuItem item) {
+		
+		onSubMenuItemClicked(item.getItemId());
+		
+		return true;
+	}
 	
-	
-//	private class MyActionProvider extends ActionProvider{
-//
-//		public MyActionProvider(Context context) {
-//			super(context);
-//			// TODO Auto-generated constructor stub
-//		}
-//
-//		@Override
-//		public View onCreateActionView() {
-//			LayoutInflater inflater = LayoutInflater.from(BaseActivity.this);
-//			View rootView = inflater.inflate(R.layout.action_submenu_create, null);
-//			
-//			
-//			return rootView;
-//		}
-//		
-//	}
+}
 
 }
--- src/com/playactionmenu/MainActivity.java
+++ src/com/playactionmenu/MainActivity.java
@@ -1,8 +1,11 @@
 package com.playactionmenu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
 
@@ -17,11 +20,23 @@ public class MainActivity extends BaseActivity {
 
 		super.onCreateOptionsMenu(menu);
 
-		int[] ids = { R.id.action_cancel, R.id.action_create };
-
-		showMenuItems(ids);
+//		int[] ids = { R.id.action_cancel, R.id.action_create };
+		String[] subMenuTitles = {"Good", "Bad"};
+		showMenuItem(R.id.action_cancel, null, subMenuTitles);
+		showMenuItem(R.id.action_create, null, null);
 
 		return true;
+	}
+	
+	
+	public void onButtonClicked(View v){
+		Intent intent = new Intent(this, SecondActivity.class);
+		startActivity(intent);
+	}
+	
+	@Override
+	protected void onSubMenuItemClicked(int itemId) {
+		Toast.makeText(this, "You've clicked: " + itemId, Toast.LENGTH_SHORT).show();
 	}
 
 }
